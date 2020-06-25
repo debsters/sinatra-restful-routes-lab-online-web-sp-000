634732ab4ae886ee8e8b9725f0ee8d338b3b0427
Deborah Barcenas <deborah.barcenas@gmail.com>
2020-06-25 20:09:36 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,12 +37,12 @@ class ApplicationController < Sinatra::Base
   end
 
   patch '/recipes/:id' do  #updates a recipe
-    @recipe = Recipe.find_by_id(params[:id])
-    @recipe.name = params[:name]
-    @recipe.ingredients = params[:ingredients]
-    @recipe.cook_time = params[:cook_time]
-    @recipe.save
-    redirect to "/recipes/#{@recipe.id}"
+    recipe = Recipe.find_by_id(params[:id])
+    recipe.name = params[:name]
+    recipe.ingredients = params[:ingredients]
+    recipe.cook_time = params[:cook_time]
+    recipe.save
+    redirect to "/recipes/#{recipe.id}"
   end
 
 
